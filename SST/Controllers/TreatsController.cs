37e87e29d05a9404e52a7140865bea7b274b4581
object-ions <moshikoatia@gmail.com>
2023-10-20 12:03:26 -0700

    Add "Index" and "Create" fx with corresponding controlls and views - for Treat.

--- SST/Controllers/TreatsController.cs
+++ SST/Controllers/TreatsController.cs
@@ -4,16 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SST.Controllers
 {
   public class TreatsController : Controller
   {
     private readonly SSTContext _db;
+    
 
     public TreatsController(SSTContext db)
     {
+      _userManager = userManager;
       _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
