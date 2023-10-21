7b073fb58e5d138e26b6c19b12ea50e18db55f65
object-ions <moshikoatia@gmail.com>
2023-10-20 18:04:53 -0700

    Add Edit and Delete to the Treat controller, add Edit View

--- SST/Controllers/TreatsController.cs
+++ SST/Controllers/TreatsController.cs
@@ -39,9 +39,16 @@ namespace SST.Controllers
     [HttpPost]
     public ActionResult Create(Treat treat)
     {
-      _db.Treats.Add(treat);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+      if (!ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
 
     public ActionResult Details(int id)
@@ -51,6 +58,35 @@ namespace SST.Controllers
                                 .ThenInclude(join => join.Flavor)
                                 .FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Treats.Update(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
