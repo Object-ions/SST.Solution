bc757af4e0d193af3e596d745e8e9cb0c29f7dec
object-ions <moshikoatia@gmail.com>
2023-10-20 19:56:21 -0700

    Add CRUD files for "Flavor" Model. add Index, Create and Details fx to model with corresponding views.

--- SST/Controllers/FlavoresController.cs
+++ SST/Controllers/FlavoresController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SST.Controllers
 {
@@ -14,6 +18,44 @@ namespace SST.Controllers
     public FlavorsController(SSTContext db)
     {
       _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> flavors = _db.Flavors.ToList();
+      return View(flavors);
+    }
+
+    //[Authorize]
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
+
+    //[Authorize]
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(flavor);
+      }
+      else
+      {
+        _db.Flavors.Add(flavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+                                .Include(flavor => flavor.JoinEntities)
+                                .ThenInclude(join => join.Treat)
+                                .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
     }
   }
 }
