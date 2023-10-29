049d33bf030ac35a9bbf9355395058522b5d26b5
object-ions <moshikoatia@gmail.com>
2023-10-28 21:28:56 -0700

    Fix bug that will link many to many releation. Fix indentation

--- SST/Models/Flavor.cs
+++ SST/Models/Flavor.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations;
 namespace SST.Models
 {
   public class Flavor
-    {
-        public int FlavorId { get; set; }
-        
-        [Required(ErrorMessage = "Creating a flavor requires a name")]
-        public string Name { get; set; }
-        
-        public List<FlavorTreat> JoinEntities { get;}
-    }
+  {
+    public int FlavorId { get; set; }
+    
+    [Required(ErrorMessage = "Creating a flavor requires a name")]
+    public string Name { get; set; }
+    
+    public List<FlavorTreat> JoinEntities { get;}
+  }
 }
